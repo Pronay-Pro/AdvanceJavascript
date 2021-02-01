ce2171c9c87fc889117af6fa015974b98bc8b541
Pronay Saha <pronaysaha788@gmail.com>
2021-02-01 22:59:07 +0600

    first

---
+++ AdvanceJavascript/TrueAndfalse.js
@@ -0,0 +1,17 @@
+//falsy;
+// 1.false;
+// 2. "";
+// 3. 0;
+// 4. undefined;
+// 5. NaN;
+// 6. Null;
+// // truly;
+// 1." ";
+// 2.{};
+// 3.'0';
+let nam = 0;
+if(nam){
+    console.log("true")
+}else{
+    console.log("false")
+}
