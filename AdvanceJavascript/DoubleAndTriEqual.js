548f240613c79978d30b27e9ddbc8fa1b08eda83
Pronay Saha <pronaysaha788@gmail.com>
2021-02-01 23:42:24 +0600

    js

---
+++ AdvanceJavascript/DoubleAndTriEqual.js
@@ -0,0 +1,14 @@
+const num1 = 2;
+const num2 = "2";
+if(num1 === num2){
+    console.log("matched")
+}else{
+    console.log("did not match")
+}
+const numb1 = 2;
+const numb2 = "2";
+if(numb1 == numb2){
+    console.log("matched")
+}else{
+    console.log("did not match")
+}
---
+++ AdvanceJavascript/NullvsUndefine.js
@@ -0,0 +1,20 @@
+// undefine
+ function cal(num1,num2){
+     console.log(num1+num2);
+     return ;
+ }
+ function cal(num1,num2){
+     console.log(num1,num2);
+     return ;
+ }
+ const result = cal(12);
+ console.log (result);
+
+ const pro ={Name:"pronay", id : "191-15-12705"};
+ console.log(pro.Name ,pro.add);
+
+ //Null
+  const pro = [2,3,4,5,6];
+  console.log(pro[12]);
+  const pro = null;
+  console.log(pro);
