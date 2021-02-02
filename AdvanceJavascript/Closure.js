a04e008efbf68c9d0b235054657e232964951b7e
Pronay Saha <pronaysaha788@gmail.com>
2021-02-02 22:34:02 +0600

    Advance

---
+++ AdvanceJavascript/Closure.js
@@ -0,0 +1,24 @@
+// function stopWatch(){
+//     let count = 0;
+//     return function(){
+//         count++;
+//         return count;
+//     }
+// }
+// const watch1 = stopWatch();
+// console.log(watch1())
+// console.log(watch1())
+// console.log(watch1())
+// console.log(watch1())
+
+if( false ){
+
+    console.log("I am not false")
+
+}
+
+else{
+
+    console.log("I am false")
+
+}
